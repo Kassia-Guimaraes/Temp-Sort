3eeb533f355706c583494d6ea17a73a0b71e9f1a
Vanessa Gomes <vanessa.gomes189@gmail.com>
2024-05-30 01:22:19 +0100

    Ficheiros adicionados com outra interpretação do problema. A alinhar na reunião de 30/05/2024

---
+++ performanceStudyFunctions.py
@@ -0,0 +1,43 @@
+import numpy as np
+import time 
+import random
+from sortingAlgorithms import *
+
+#TODO:
+#add default case or Use empty array as error code 
+
+def createArray(distribution,length):
+    V = []
+
+    for j in range(1,length+1):
+        if (distribution == "random"):
+            V += [random.randrange(1,101)]
+        if (distribution == "poisson"):
+            V += [np.random.poisson(100)]
+        if (distribution == "binomial"):
+            V += [np.random.binomial(100,0.5)]
+        if (distribution == "geometric"):
+            V += [np.random.geometric(0.01)]
+            
+    return V
+  
+def calcExecutionTime(algorithm, array,output, gaps = []):
+    tot = 0.0
+    st = time.time_ns() / (10 ** 9) * 1000
+    if(algorithm == "selectionSort"):  
+        selectionSort(array)
+    if(algorithm == "insertionSort"):  
+        insertionSort(array)
+    if(algorithm == "shellSort"):  
+        shellSort(array, gaps)
+    if(algorithm == "quickSort"):  
+        quickSort(array,0,len(array)-1)
+    if(algorithm == "countingSort"):  
+        countingSort(array,len(array)-1, max(array))
+    en = time.time_ns() / (10 ** 9) * 1000
+    tot += (en-st)
+    if output == True:
+        print(algorithm + " execution time = "+str(tot)+" ms")
+    return tot
+    
+
